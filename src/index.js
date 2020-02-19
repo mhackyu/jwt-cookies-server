df25c078aa2b6b1ccad59b6191c10f46215cf0d8
mhackyu <mark.christian.paderes@gmail.com>
2020-02-19 17:02:51 +0800

    FIX: Add env for client app

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || '3000';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
